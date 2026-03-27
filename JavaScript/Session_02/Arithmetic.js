10d20e3abab639057f597482b9fc319c401798c1
Shivani Surve <surveshivani99@gmail.com>
2026-03-27 16:06:09 +0530

    Add files via upload

---
+++ JavaScript/Session_02/Arithmetic.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 20;
+
+console.log("Sum",a+b,".");
+console.log("Sub",a-b,".");
+console.log("Mul",a*b,".");
+console.log("Div",a/b,".");
+console.log("Mod",a%b,".");
+
+
+// in c 
+// int num = 10;
+//  printf("Num = %d",num);
