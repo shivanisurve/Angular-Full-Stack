63a7293b895a21c9a225ab34dd88c77b8bd06ae9
Shivani Surve <surveshivani99@gmail.com>
2026-07-05 14:25:04 +0530

    Add files via upload

---
+++ JavaScript/Inline,External,Internal/External.js
@@ -0,0 +1,7 @@
+// Select the button
+const button = document.getElementById("myButton");
+
+// Add click event
+button.addEventListener("click", function() {
+    alert("Button was clicked!");
+});
